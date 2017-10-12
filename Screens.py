53bbe55485cea618ce15bc5b53fa2e3cf92f5761
madison <madison.neal@kearsrage.org>
2017-10-12 13:17:21 -0400

    agian

--- Screens.py
+++ Screens.py
@@ -2,11 +2,11 @@ def splash(debug = False):
 	if debug: print "--In splash function--"
 	output = ""
 	output += "***************************************\n"
-	output += "*                                     *\n"
-	output += "*        Welcome to Madlibs           *\n"
-	output += "*           Written by                *\n"
-	output += "*         Chris Spooner               *\n"
-	output += "*                                     *\n"
+	output += "^                                     ^\n"
+	output += "^        Welcome to Madlibs           ^\n"
+	output += "^           Written by                ^\n"
+	output += "^  Madison Neal and Mike Schubert     ^\n"
+	output += "^                                     ^\n"
 	output += "***************************************\n"
 	return output
 
